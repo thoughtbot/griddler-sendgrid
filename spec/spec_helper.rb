063c3e9752192052ac6e2534b85e62999bd8d2e2
Gabe Berke-Williams <gabebw@gabebw.com>
2016-09-11 15:17:59 -0700

    Enable both `should` and `expect` in RSpec
    griddler-sendgrid uses `should` everywhere, but the shared expectation from
    Griddler uses `expect`. To prevent warnings in newer versions of RSpec, enable
    both syntaxes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,7 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.order = 'random'
   config.include Griddler::Testing
+  config.expect_with(:rspec) do |c|
+    c.syntax = [:should, :expect]
+  end
 end
